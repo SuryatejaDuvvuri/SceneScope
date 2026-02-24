2b22d16f2a764ed2b3d7bdb83912b0e33602c39e
Suryateja Duvvuri <duvvurisuryateja95@gmail.com>
2026-02-23 20:20:39 -0800

    Create queries and high level architecture flow

--- scripts/db.sql
+++ scripts/db.sql
@@ -1,9 +1,37 @@
-CREATE projects
+CREATE TABLE projects
 (
-    id UUID PRIMARY_KEY
+    id UUID PRIMARY KEY,
     title VARCHAR(255),
     genre VARCHAR(50),
     time_period VARCHAR(255),
+    films TEXT,
+    tone TEXT,
     created_at VARCHAR(255),
     updated_at VARCHAR(255)
+);
+
+CREATE TABLE characters
+(
+    id UUID PRIMARY KEY,
+    projectId UUID REFERENCES projects(id),
+    name TEXT,
+    role TEXT,
+    age TEXT,
+    description TEXT,
+    image_url URL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE scenes
+(
+    id TEXT PRIMARY KEY,
+    projectId UUID REFERENCES projects(id),
+    scene INTEGER,
+    heading TEXT,
+    description TEXT,
+    mood TEXT,
+    image_desc TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
