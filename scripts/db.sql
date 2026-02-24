d986bcecfd41cacb23899572380a8fbe7c4a5456
Suryateja Duvvuri <duvvurisuryateja95@gmail.com>
2026-02-23 20:01:23 -0800

    Setup project structure

---
+++ scripts/db.sql
@@ -0,0 +1,9 @@
+CREATE projects
+(
+    id UUID PRIMARY_KEY
+    title VARCHAR(255),
+    genre VARCHAR(50),
+    time_period VARCHAR(255),
+    created_at VARCHAR(255),
+    updated_at VARCHAR(255)
+)
